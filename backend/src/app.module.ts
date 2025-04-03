437de7f4747d57f0f1d8634e6e23a56e7bf477aa
oleg <zustzero123@gmail.com>
2025-04-03 10:57:01 +0300

    init

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mongodb',
+        url: configService.get<string>('MONGO_URI'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        ssl: true,
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      }),
+    }),
+    ProductsModule,
+    UsersModule,
+    AuthModule
+  ],
 })
 export class AppModule {}
