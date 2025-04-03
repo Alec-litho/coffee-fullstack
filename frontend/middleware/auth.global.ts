ed43182eb0c77e2a7f2c6c20fe37300e272d7115
oleg <zustzero123@gmail.com>
2025-04-03 10:59:23 +0300

    middleware created

---
+++ frontend/middleware/auth.global.ts
@@ -0,0 +1,15 @@
+import { useAuthStore } from "~/stores/auth.store";
+
+export default defineNuxtRouteMiddleware((to) => {
+  
+  const authStore = useAuthStore()
+  authStore.initializeAuth()
+
+  if (to.path === '/login' && authStore.isAuthenticated) {
+    return navigateTo('/products')
+  }
+
+  if (to.path !== '/login' && !authStore.isAuthenticated) {
+    return navigateTo('/login')
+  }
+})
